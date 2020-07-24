b930812b7fc441ffcf833ae73a2e801980aea0ec
Tauzer <tauzersilver@gmail.com>
2020-07-24 15:25:24 +0300

    Update metrika.js

--- metrika.js
+++ metrika.js
@@ -11,8 +11,12 @@
 
 
 
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172948527-1"></script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-172948527-1');
+
